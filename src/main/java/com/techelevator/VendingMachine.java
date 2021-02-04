108bec24f5ccde6accbe9bca94e3046588266fc0
ShiraHenteleff <Shiranhenteleff@gmail.com>
2021-02-04 12:32:03 -0500

    started creating methods

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,4 +1,40 @@
 package com.techelevator;
 
+import java.util.ArrayList;
+
 public class VendingMachine {
+    //FIELDS
+    private ArrayList<Slot> slots;
+
+    //METHODS
+    //This is temporary. Will be replaced by constructor that parses from file.
+    public VendingMachine(ArrayList<Slot> slots) {
+        this.slots = slots;
+    }
+
+    //Prints Menu of all available items.
+    public void printMenu(){
+        for(Slot s: slots){
+                System.out.println(s);
+        }
+    }
+
+    //Returns item from selected slot, it slot exists.
+    //Otherwise, if item exists but is sold out, it prints that and returns null.
+    //if item does not exist, it prints that and returns null.
+    public buyable buyItem(String slotName){
+        for (Slot s: slots) {
+            if(slotName.equals(s.getName())&&s.itemsSize()>0){
+                return s.dispense();
+            }
+            else if(slotName.equals(s.getName())&&s.itemsSize()==0){
+                System.out.println("Item is sold out. Sorry.");
+                return null;
+            }
+        }
+        System.out.println("That item doesn't exist!");
+        return null;
+    }
+
+
 }
