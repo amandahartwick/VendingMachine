14a2f464f09a88fa6f9ba96646119807c87c9096
ShiraHenteleff <Shiranhenteleff@gmail.com>
2021-02-04 11:38:40 -0500

    Created Slot and SlotTests

--- src/main/java/com/techelevator/Slot.java
+++ src/main/java/com/techelevator/Slot.java
@@ -1,4 +1,33 @@
 package com.techelevator;
 
+import java.util.ArrayList;
+
 public class Slot {
+    private String name;
+    private ArrayList<buyable> items;
+
+    public Slot(String name, buyable item) {
+        this.name = name;
+        items = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            items.add(item);
+        }
+    }
+
+    public int itemsSize(){
+        return items.size();
+    }
+
+    public buyable dispense(){
+       if(!items.isEmpty()) {
+           buyable output = items.get(0);
+           items.remove(0);
+           return output;
+       }
+       return null;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
--- src/test/java/com/techelevator/SlotTests.java
+++ src/test/java/com/techelevator/SlotTests.java
@@ -1,4 +1,25 @@
 package com.techelevator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SlotTests {
+
+    @Test
+    public void constructor_creates_five_of_item() {
+        buyable item = new Candy();
+        Slot slot = new Slot("A1",item);
+
+        Assert.assertEquals(5, slot.itemsSize());
+    }
+
+    @Test
+    public void dispense_removes_item_and_returns_it() {
+        buyable item = new Candy();
+        Slot slot = new Slot("A1",item);
+        buyable anotherItem = slot.dispense();
+
+        Assert.assertEquals(4,slot.itemsSize());
+        Assert.assertEquals(item, anotherItem);
+    }
 }
