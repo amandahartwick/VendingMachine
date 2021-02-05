5ce9538b9cff5cbbb31f66cc5d34124a1174f848
Amanda Hartwick <amandarhartwick@gmail.com>
2021-02-05 10:13:27 -0500

    account tests in progress

--- src/test/java/com/techelevator/AccountTests.java
+++ src/test/java/com/techelevator/AccountTests.java
@@ -1,4 +1,18 @@
 package com.techelevator;
 
 public class AccountTests {
+	
+	@Test
+	public void giveChange_returns_correct_change_in_coins() {
+		 Account a = new Account();
+		 
+		 double balance = 47;
+		 
+		 int output = balance.giveChange();
+		 
+		 
+		
+	}
+	
+
 }
